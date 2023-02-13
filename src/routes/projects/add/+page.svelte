127196e3df31ea7e4bb747aa8007764924d8928d
Isa <isa.ya@mail.ru>
2023-02-13 17:39:32 +0300

    Agree with terms before adding new project;

--- src/routes/projects/add/+page.svelte
+++ src/routes/projects/add/+page.svelte
@@ -5,6 +5,7 @@
 	import Select from '$lib/components/select.svelte'
 	import Input from '$lib/components/input.svelte'
 
+	let agreeWithTerms = false
 	let project = {
 		name: '',
 		region: '',
@@ -39,48 +40,55 @@
 	}
 </script>
 
-<div class="my-10 mx-auto max-w-xl text-center text-lg">
-	Уважаемый инициатор инвестиционного проекта!
-	<br>
-	Для проведения оценки вашего проекта предлагаем заполнить анкету, содержащую набор необходимых показателей.
-	<br>
-	Нажимая кнопку "Продолжить", вы соглашаетесь с обработкой представленных данных.
-	АО «Корпорация Туризм.РФ» гарантирует безопасность представленных данных.
-</div>
-<div class="flex items-center justify-between my-10 px-3 md:px-10">
-	<div class="text-2xl md:text-3xl">Новый проект</div>
-	<a href="/projects" class="btn btn-outline">Закрыть</a>
-</div>
-<div class="flex flex-col gap-2">
-	<Input name="theProjectName"
-	       label="Название"
-	       placeholder="Название проекта"
-	       bind:value={project.name}/>
-	<Select name="theProjectRegion"
-	        label="Регион"
-	        title="Выберите регион"
-	        options={$DIRs['regions']?.values}
-	        bind:value={project.region}
-	/>
-	<Select name="theProjectBuildingType"
-	        label="Тип объекта"
-	        title="Выберите тип объекта"
-	        options={$DIRs['buildingTypes']?.values}
-	        bind:value={project.buildingType}
-	/>
-	<Select name="theProjectBuildingCategory"
-	        label="Категория объекта"
-	        title="Выберите категорию объекта"
-	        options={$DIRs['buildingCategory']?.values}
-	        defaultDisabled={false}
-	        bind:value={project.buildingCategory}
-	/>
-</div>
-<div class="flex justify-end mt-10">
-	<button class="btn btn-outline btn-secondary"
-	        on:click={addProject}
-	        class:pointer-events-none={!project?.name.trim()}
-	        class:opacity-50={!project?.name.trim()}>
-		Продолжить
-	</button>
-</div>
+{#if agreeWithTerms}
+	<div class="flex items-center justify-between my-10 px-3 md:px-10">
+		<div class="text-2xl md:text-3xl">Новый проект</div>
+		<a href="/projects" class="btn btn-outline">Закрыть</a>
+	</div>
+	<div class="flex flex-col gap-2">
+		<Input name="theProjectName"
+		       label="Название"
+		       placeholder="Название проекта"
+		       bind:value={project.name}/>
+		<Select name="theProjectRegion"
+		        label="Регион"
+		        title="Выберите регион"
+		        options={$DIRs['regions']?.values}
+		        bind:value={project.region}
+		/>
+		<Select name="theProjectBuildingType"
+		        label="Тип объекта"
+		        title="Выберите тип объекта"
+		        options={$DIRs['buildingTypes']?.values}
+		        bind:value={project.buildingType}
+		/>
+		<Select name="theProjectBuildingCategory"
+		        label="Категория объекта"
+		        title="Выберите категорию объекта"
+		        options={$DIRs['buildingCategory']?.values}
+		        defaultDisabled={false}
+		        bind:value={project.buildingCategory}
+		/>
+	</div>
+	<div class="flex justify-end mt-10">
+		<button class="btn btn-outline btn-secondary"
+		        on:click={addProject}
+		        class:pointer-events-none={!project?.name.trim()}
+		        class:opacity-50={!project?.name.trim()}>
+			Сохранить
+		</button>
+	</div>
+{:else}
+	<div class="my-10 mx-auto text-center text-xl">
+		Уважаемый инициатор инвестиционного проекта!
+		<br>
+		Для проведения оценки вашего проекта предлагаем заполнить анкету, содержащую набор необходимых показателей.
+		<br>
+		Нажимая кнопку "Продолжить", вы соглашаетесь с обработкой представленных данных.
+		АО «Корпорация Туризм.РФ» гарантирует безопасность представленных данных.
+	</div>
+	<div class="flex items-center justify-center gap-10 my-10 px-3 md:px-10">
+		<a href="/projects" class="btn btn-outline">Закрыть</a>
+		<button class="btn btn-outline" on:click={() => agreeWithTerms = true}>Продолжить</button>
+	</div>
+{/if}
